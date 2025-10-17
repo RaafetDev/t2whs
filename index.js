d9a11b712e3e4d0dcd7f06c9e658141a020adde8
RaafetDev <121636624+RaafetDev@users.noreply.github.com>
2025-10-17 04:11:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ const DEFAULT_CONFIG = {
     host: 'ttcbgkpnl6at7dqhroa2shu44zqxzpwwwvdxbzoqznxk7lg5xso6bbqd.onion'
   },
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
+    //'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0',
+    //'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    /*'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate',
     'Upgrade-Insecure-Requests': '1',
     'Sec-Fetch-Site': 'none',
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-User': '?1',
-    'DNT': '1'
+    'DNT': '1'*/
   }
 };
 
